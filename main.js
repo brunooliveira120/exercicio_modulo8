a7c9ca10bde46147aa5344db70f610d678596678
Bruno Oliveira <brunoo.oliveira120@gmail.com>
2023-04-03 18:32:49 -0300

    commit final

--- main.js
+++ main.js
@@ -1,41 +1,38 @@
-const form = document.getElementById("form-agenda")
-var linhas = ""
-const nomes = []
-const numeros = []
+const form = document.getElementById("form-agenda");
+const nomes = [];
+const numeros = [];
+var linhas = '';
 
-
-form.addEventListener("submit", function(e){
-    e.preventDefault()
+form.addEventListener('submit', function(e){
+    e.preventDefault();
 
     adicionaLinha()
     atualizaTabela()
 })
 
-function adicionaLinha() {
-    const inputNomeContato = documento.getElementById("nome-contato");
-    const inputNumeroContato = documento.getElementById("numero-contato");
+function adicionaLinha(){
+    const inputNomecontato = document.getElementById('nome-contato');
+    const inputNumerocontato = document.getElementById('numero-contato');
 
-    if(nomes.includes(inputNomeContato.value)){
-        alert(`O Contato: ${inputNomeContato.value} já está cadastrado`)
+    if(nomes.includes(inputNomecontato.value)) {
+        alert(`O contato ${inputNomecontato.value} já foi inserido`)
     } else {
-        nomes.push(inputNomeContato.value)
-        numeros.push(parseFloat(inputNumeroContato.value))
+        nomes.push(inputNomecontato.value)
+        numeros.push(parseFloat(inputNumerocontato.value));
 
         var linha = `<tr>`
-
-        linha += `<td>${inputNomeContato.value}</td>`
-        linha += `<td>${inputNumeroContato.value}</td>`
+        linha += `<td>${inputNomecontato.value}</td>`
+        linha += `<td>${inputNumerocontato.value}</td>`
         linha += `</tr>`
 
-        linhas += linha
-
+        linhas += linha;
     }
 
-    inputNomeContato.value = ""
-    inputNumeroContato.value = ""
+    inputNomecontato.value = '';
+    inputNumerocontato.value = '';
 }
 
 function atualizaTabela(){
-    const corpoTabela = document.querySelector("tbody")
-    corpoTabela.innerHTML = linhas
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
 }
